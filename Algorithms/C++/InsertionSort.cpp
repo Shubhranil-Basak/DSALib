1d48962e0cbde6fbc2971ebbf5e450964fbe41b1
Shubhranil <Shubhranil.Basak@iiitb.ac.in>
2024-02-02 16:47:37 +0530

    Added Insertion Sort

---
+++ Algorithms/C++/InsertionSort.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void InsertionSort(int arr[], int n){
+	int i, key, j;
+	for (i = 1; i < n; i++) {
+		key = arr[i];
+		j = i - 1;
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
+
+void PrintArray(int arr[], int size){ 
+	int i; 
+	for (i = 0; i < size; i++) { 
+		cout << arr[i] << " "; 
+	}
+    cout << endl; 
+}
+
+int main(){
+    int n;
+	cin >> n;
+    int arr[n];
+    for(int  i=0;i<n;i++){
+        cin >> arr[i];
+    }
+	InsertionSort(arr, n); 
+	cout << "Sorted array: \n"; 
+	PrintArray(arr, n); 
+	return 0; 
+}
