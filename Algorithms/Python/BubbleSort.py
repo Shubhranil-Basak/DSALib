5594e98f979e30ded3b272d3a9a42b1225c1f4e1
Shubhranil <Shubhranil.Basak@iiitb.ac.in>
2024-02-02 14:51:31 +0530

    Added Bubble Sort

--- Algorithms/Python/BubbleSort.py
+++ Algorithms/Python/BubbleSort.py
@@ -2,10 +2,10 @@ def BubbleSort(arr: list) -> list:
     length = len(arr)
     for i in range(length):
         for j in range(length - 1):
-            if arr[j] > arr[j - 1]:
+            if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return  arr
 
 if __name__ == '__main__':
-    a = list(int,  input("Enter the elements of array separated by space : ").split())
+    a = list(map(int,  input("Enter the elements of array separated by space : ").split()))
     print("The sorted array is ",BubbleSort(a))
